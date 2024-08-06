3ab1e4502c0441068bd0e139d8b5df7fb3690832
Bruno Ribeiro <brunoro343@gmail.com>
2024-08-06 15:25:49 -0300

    Update python.py
    mudei um pouco o código pra tentar consertar um problema, mas não funcionou.
    o problema é que quando o usuário seleciona a opção 2 na linha 17, o programa não aciona a função. se alguêm puder me ajudar eu agradeço

--- python.py
+++ python.py
@@ -1,20 +1,19 @@
+rps = int
+
 def addlogin():
     id = input("Insira qual aplicativo/site irá usar o login:")
     name = input("Insira o nome do usuário:")
     password = input("Insira a senha do usuário")
     outro = input("Gostaria de colocar alguma descrição do login?:").lower()
     if outro == "sim":
-
-        print("Digite a descrição:")
+        print("descrição")
 while True:
-        senha =int(input("Bem vindo bruno, por favor, coloque sua senha:"))
-        if senha == 12345:
-            print("GERENCIADOR DE LOGINS")
+    senha=int(input("Bem vindo bruno, por favor, coloque sua senha:"))
+    if senha == 12345:
+            print("GERENCIADOR DE LOGINS") # a partir dessa linha até a linha 16 é o menu principal 
             print("1.Checar login\n")
             print("2.Adicionar login\n")
             print("3.Deletar login")
             rps = input("Digite sua opção:")
-        elif rps == 2:
-            addlogin()    
-        else:
-            print("Senha inválida")
+            if rps == 2:
+                addlogin()
